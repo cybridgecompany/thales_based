e51ddbaef81507ad8339f7732e4c52221fdfbd02
cybridgecompany <cybridgecontactus@gmail.com>
2020-09-23 19:23:41 +0300

    sucess

--- js/small-business.js
+++ js/small-business.js
@@ -1043,3 +1043,13 @@ jQuery(document).ready(function($) {
             });
     }
 });
+
+var $window = $(window),
+    $html = $('#menu-bar');
+
+$window.resize(function resize() {
+  if ($window.width() < 768) {
+    return $html.removeClass('nav-stacked');
+  }
+  $html.addClass('nav-stacked');
+}).trigger('resize');
